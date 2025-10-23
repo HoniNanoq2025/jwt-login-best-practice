c39a8127174ba76a1bc62b3f35a6cc0cfe9a5896
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-10-23 10:05:25 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import styles from "./App.modules.css";
+
 export default function App() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [token, setToken] = useState("");
-
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 }
